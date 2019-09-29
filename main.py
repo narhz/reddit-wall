86d5590748341906eb99cb22370a01f6029a413d
narhz <narhz@pm.me>
2019-09-29 18:07:35 -0500

    add function to auth with reddit

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ def config():
         config.read('config.ini')
 
     return config['REDDIT']
+
+
+def auth_reddit():
+    config = config()
+
+    reddit = praw.Reddit(client_id=config['client_id'],
+                     client_secret=config['client_secret'],
+                     user_agent=config['user_agent'])
+
+    return reddit

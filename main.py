187aa13bc18e650588ed0ba36232877c4fcfb19c
narhz <narhz@pm.me>
2019-09-29 18:17:48 -0500

    return whole config object, add get_user_subs

--- main.py
+++ main.py
@@ -11,14 +11,26 @@ def config():
     else:
         config.read('config.ini')
 
-    return config['REDDIT']
+    return config
 
 
 def auth_reddit():
-    config = config()
+    config = config()['REDDIT']
 
     reddit = praw.Reddit(client_id=config['client_id'],
                      client_secret=config['client_secret'],
                      user_agent=config['user_agent'])
 
     return reddit
+
+
+def get_user_subs():
+    if os.path.exists('dev_subs.txt'):
+        with open('dev_subs.txt', 'r') as subs_file:
+            subs_from_file = subs_file.readlines()
+
+        subs = []
+        for i in subs_from_file:
+            subs.append(i.replace('\n', ''))
+
+    return subs
